34d6910b4ac124aba21891facca4796d0ae0ff3d
柱子 <wenmin92@gmail.com>
2021-10-04 11:38:49 +0800

    Update bubei.js

--- scripts/bubei.js
+++ scripts/bubei.js
@@ -2,9 +2,9 @@
 https://sapi.beingfine.cn/v3/report/launch/
 sapi.beingfine.cn
 */
-const body = $response.body;
+let body = $response.body;
 
-body.replace(/(\\"granted\\"):\d/g, '$1:1');
-body.replace(/\\"expire_date\\":]d+/g, '$1:4079167311000');
+body = body.replace(/(\\"granted\\"):\d/g, '$1:1');
+body = body.replace(/(\\"expire_date\\"):\d+/g, '$1:4079167311000');
 
 $done(body);
